5058c2680ef7b95fdc8dd7d71f9d3d46b3894080
DavidRagone <dmragone@gmail.com>
2017-06-24 12:59:08 -0700

    Filter files to only specs needed to run

--- ruby/david-spec/lib/david_spec/spec.rb
+++ ruby/david-spec/lib/david_spec/spec.rb
@@ -1,11 +1,20 @@
+require 'find'
+
 module DavidSpec
   class Spec
     # Default pattern for spec files.
     DEFAULT_PATTERN = 'spec/**{,/*/**}/*_spec.rb'
 
     def self.run(pattern=nil)
-      puts 'runnnnnnnning'
-      puts pattern
+      current_directory = Dir.pwd
+      spec_files = Find.find(current_directory).grep(/#{current_directory}\/(test|spec).+\.rb/)
+      spec_files.each { |file| Execute.(file) }
+    end
+  end
+
+  class Execute
+    def self.call(file)
+      puts "executing #{file}"
     end
   end
 end
